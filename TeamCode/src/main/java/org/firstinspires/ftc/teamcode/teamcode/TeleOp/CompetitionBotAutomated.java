e9c80a8388b6b061d63c0fba96ef75fc52980957
Ariella Navarro <aya.navarro@me.com>
2017-12-10 14:22:57 -0800

    Revised TeleOp Automated

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/TeleOp/CompetitionBotAutomated.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teamcode/TeleOp/CompetitionBotAutomated.java
@@ -7,8 +7,10 @@ import com.qualcomm.robotcore.hardware.DcMotor;
 import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.hardware.Servo;
 
+import static java.lang.Thread.sleep;
+
 @TeleOp(name="CompetitionBot", group="Pushbot")
-public class CompetitionBot extends OpMode {
+public class CompetitionBotAutomated extends OpMode {
 
     // Motors
     public DcMotor frontRight;
@@ -39,6 +41,8 @@ public class CompetitionBot extends OpMode {
         lift = hardwareMap.dcMotor.get("lift");
         intakeRight = hardwareMap.dcMotor.get("intakeRight");
         intakeLeft = hardwareMap.dcMotor.get("intakeLeft");
+
+        lift.setMode(DcMotor.RunMode.RUN_USING_ENCODER);
 
         s1 = hardwareMap.servo.get("s1");
         s2 = hardwareMap.servo.get("s2");
@@ -114,37 +118,39 @@ public class CompetitionBot extends OpMode {
         if (gamepad1.left_trigger > .1) {
             intakeLeftPower = gamepad1.left_trigger;
             catcherLeft.setPosition(1);
-            catcherRight.setPosition(1);
+            catcherRight.setPosition(1); // use catchers
+            s1.setPosition(1);
+            s2.setPosition(1); //open arms when taking in glyphs
         }
         else if (gamepad1.left_bumper)
         {
-            intakeLeftPower = -0.7;
+            intakeLeftPower = -0.7; // reverse blocks
         }
         else {
             intakeLeftPower = 0;
         }
 
-        if(gamepad1.x) {
-            arm.setPosition(1);
+        if(gamepad1.a) {
+            arm.setPosition(1); // jewel arm
         }
 
-        if(gamepad1.y){
+        if(gamepad1.b){
             arm.setPosition(0);
         }
 
         // Gamepad 2 controls
         if (Math.abs(gamepad2.left_stick_y) > .1 ) {
-            liftPower = gamepad2.left_stick_y;
+            liftPower = gamepad2.left_stick_y; // set power to lift
         }
 
         if (gamepad2.right_trigger > 0.1) {
             s1.setPosition(.5);
-            s2.setPosition(.5);
+            s2.setPosition(.5); // probably close arms
         }
 
         if (gamepad2.left_trigger > 0.1) {
             s1.setPosition(1);
-            s2.setPosition(1);
+            s2.setPosition(1); // open arms
         }
 
         if (gamepad2.right_bumper) {
@@ -155,6 +161,42 @@ public class CompetitionBot extends OpMode {
             catcherLeft.setPosition(0); // move away catchers
             catcherRight.setPosition(0);
         }
+
+        if (gamepad2.x) { //GRABBING ONE BLOCK
+            s1.setPosition(.5); // close arms to grab block
+            s2.setPosition(.5);
+            lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER); // reset encoders
+            lift.setTargetPosition(1600); // carries glyph to position 1600
+            lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            lift.setPower(0.5); // half speed
+        }
+
+        if (gamepad2.y) { //RETRIEVE TWO BLOCKS
+            s1.setPosition(1); // let go of glyphs
+            s2.setPosition(1);
+            lift.setTargetPosition(0); // maybe reverse to starting position
+            lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            lift.setPower(0.5);
+            s1.setPosition(0.5); //grab glyphs
+            s2.setPosition(0.5);
+            lift.setTargetPosition(750);
+            lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            lift.setPower(0.5);
+            catcherLeft.setPosition(0);
+            catcherRight.setPosition(0);
+        }
+
+        if (gamepad2.b) { //RELEASE BLOCKS
+            catcherLeft.setPosition(0);
+            catcherRight.setPosition(0);
+            s1.setPosition(1);
+            s2.setPosition(1);
+            lift.setTargetPosition(0);
+            lift.setMode(DcMotor.RunMode.RUN_TO_POSITION);
+            lift.setPower(0.5);
+            lift.setMode(DcMotor.RunMode.STOP_AND_RESET_ENCODER);
+        }
+
 
         // Set powers
         lift.setPower(liftPower);
